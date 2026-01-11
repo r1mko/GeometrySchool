85e9838f81e33baa62fe3790506953c9843c4d85
r1mko <r1mko@yahoo.com>
2026-01-11 18:21:16 +0400

    ограничиваем анимацию

--- Assets/Scripts/Boss/Controllers/BossTeacherController.cs
+++ Assets/Scripts/Boss/Controllers/BossTeacherController.cs
@@ -35,7 +35,7 @@ public class BossTeacherController : MonoBehaviour
             for (int i = 0; i < 1; i++)
             {
                 //rayController.ShotRay(RayType.Target);
-                trapController.TriggerTrapPlacement(TrapType.Static);
+                trapController.TriggerTrapPlacement(TrapType.Falling);
 
             }
         }
--- Assets/Scripts/Boss/Controllers/TrapController.cs
+++ Assets/Scripts/Boss/Controllers/TrapController.cs
@@ -198,7 +198,10 @@ public class TrapController : MonoBehaviour
         selectedCell.isBlocked = true;
 
         GameObject trapObj = Instantiate(system.trapPrefab, selectedCell.spawnPoint.position, Quaternion.identity);
-        ActionBus.InvokeSpawnStaticTrap(selectedCell.spawnPoint.position);
+        if (system.type == TrapType.Static)
+        {
+            ActionBus.InvokeSpawnStaticTrap(selectedCell.spawnPoint.position);
+        }
         if (trapObj.TryGetComponent<TrapBehaviour>(out var trap))
         {
             trap.Init(player.transform, null, selectedCell.spawnPoint.position, system.type);
